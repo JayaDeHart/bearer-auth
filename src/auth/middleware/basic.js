d87ef769df7035c1a2c9b1eb15e7d8c2e3426e09
Jaya DeHart <dehartj97@gmail.com>
2021-08-24 20:24:47 -0700

    ready to submit

--- src/auth/middleware/basic.js
+++ src/auth/middleware/basic.js
@@ -1,17 +1,22 @@
 'use strict';
 
 const base64 = require('base-64');
-const { user } = require('../models/index.js')
+const { users } = require('../models/index.js')
 
 module.exports = async (req, res, next) => {
 
   if (!req.headers.authorization) { return _authError(); }
 
-  let basic = req.headers.authorization;
-  let [username, pass] = base64.decode(basic).split(':');
+  let info = req.headers.authorization.split(" ")[1];
+  let basic = base64.decode(info);
+  let username = basic.split(":")[0];
+  let password = basic.split(":")[1];
+
+
+
 
   try {
-    req.user = await user.authenticateBasic(username, pass)
+    req.user = await users.authenticateBasic(username, password)
     next();
   } catch (e) {
     res.status(403).send('Invalid Login');
--- src/auth/routes.js
+++ src/auth/routes.js
@@ -7,14 +7,17 @@ const { users } = require('./models/index.js');
 const basicAuth = require('./middleware/basic.js')
 const bearerAuth = require('./middleware/bearer.js')
 
+
 authRouter.post('/signup', async (req, res, next) => {
+
   try {
     let userRecord = await users.create(req.body);
+
     const output = {
-      user: userRecord,
+      user: { id: userRecord.id, username: userRecord.username },
       token: userRecord.token
     };
-    res.status(200).json(output);
+    res.status(201).json(output);
   } catch (e) {
     next(e.message);
   }
@@ -22,15 +25,15 @@ authRouter.post('/signup', async (req, res, next) => {
 
 authRouter.post('/signin', basicAuth, (req, res, next) => {
   const user = {
-    user: request.user,
-    token: request.user.token
+    user: { id: req.user.id, username: req.user.username },
+    token: req.user.token
   };
   res.status(200).json(user);
 });
 
 authRouter.get('/users', bearerAuth, async (req, res, next) => {
-  const users = await Users.findAll({});
-  const list = users.map(user => user.username);
+  const userslist = await users.findAll();
+  const list = userslist.map(user => user.username);
   res.status(200).json(list);
 });
 
